a3685bd59caf6b1bb12e85c39bb38d8874101882
maggiemayvan <vancle617@gmail.com>
2016-04-06 10:43:11 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,2 @@
 console.log('This would be the main JS file.');
+var name= prompt('What is your first name?'); var title=prompt('When you grow up, you want to be a:'); alert("Hello " +name+ " the world's future most extraordinary "+ title+ "!");
